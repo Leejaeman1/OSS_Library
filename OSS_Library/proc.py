46babdf02547afc3689bff647d0d370ad8a0fe33
Jeon YoungJin <smro003@hanyang.ac.kr>
2024-12-01 19:08:27 +0900

    볼륨 조절 함수 사용중 오류 발생가능 요소 제거

--- OSS_Library/proc.py
+++ OSS_Library/proc.py
@@ -7,20 +7,23 @@ import math
 #+6dB은 원래 오디오의 2배 볼륨
 #-6dB은 원래 오디오의 1/2배 볼륨
 def volumeAdjustment(input_audio,volume_persent):
-    
-    if volume_persent>200:
-        print("너무 큰 볼륨 (200%이상)")
-        return 0
-    
-    elif volume_persent<0:
-        print("볼륨(%)에 음수가 입력됨")
-        return 0
-    
-    elif volume_persent==0:
-        adjusted_audio=(input_audio-math.inf).normalize()
+    try:
+        if volume_persent>200:
+            print("너무 큰 볼륨이 요청되었습니다. (0 ~ 200)")
+            return None
+        
+        elif volume_persent<0:
+            print("볼륨(%)에 음수가 입력되었습니다. (0 ~ 200)")
+            return None
+        
+        elif volume_persent==0:
+            adjusted_audio=AudioSegment.silent(duration=len(input_audio))
 
-    else:
-        volume_dB = 20*math.log10(volume_persent/100)
-        adjusted_audio=(input_audio+volume_dB).normalize()
+        else:
+            volume_dB = 20*math.log10(volume_persent/100)
+            adjusted_audio=input_audio+volume_dB
 
-    return adjusted_audio
+        return adjusted_audio
+    except:
+        print("볼륨 조절 함수 volumeAdjustment() 사용중에 오류가 발생하였습니다.")
+        return None
