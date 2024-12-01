0d79bd3996630aa011385ccaee5508a8140db176
Jeon YoungJin <smro003@hanyang.ac.kr>
2024-12-01 17:51:57 +0900

    볼륨 조절기능 범위 수정 (0% ~ 200%)

--- OSS_Library/proc.py
+++ OSS_Library/proc.py
@@ -1,6 +1,6 @@
 #볼륨 관련 모듈
 from pydub import AudioSegment
-
+import math
 
 #볼륨 조절 기능함수
 #오디오와 볼륨(퍼센트)로 입력받음
@@ -8,13 +8,19 @@ from pydub import AudioSegment
 #-6dB은 원래 오디오의 1/2배 볼륨
 def volumeAdjustment(input_audio,volume_persent):
     
-    volume_dB = 6*(volume_persent/100)
-    if (volume_dB>6):
-        print("너무 큰 볼륨")
+    if volume_persent>200:
+        print("너무 큰 볼륨 (200%이상)")
         return 0
-    elif (volume_dB<-6):
-        print("너무 작은 볼륨")
+    
+    elif volume_persent<0:
+        print("볼륨(%)에 음수가 입력됨")
         return 0
+    
+    elif volume_persent==0:
+        adjusted_audio=(input_audio-math.inf).normalize()
+
     else:
+        volume_dB = 20*math.log10(volume_persent/100)
         adjusted_audio=(input_audio+volume_dB).normalize()
-        return adjusted_audio
+
+    return adjusted_audio
