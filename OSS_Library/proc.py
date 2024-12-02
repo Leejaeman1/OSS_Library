8ed96a8fd548bf1b814a9cc8401e39cf4a996943
Jeon YoungJin <smro003@hanyang.ac.kr>
2024-12-02 10:20:24 +0900

    볼륨 조절 예외부분 완성

--- OSS_Library/proc.py
+++ OSS_Library/proc.py
@@ -1,43 +1,36 @@
-#볼륨 관련 모듈
 from pydub import AudioSegment
 import math
 
-#볼륨 조절 기능함수
-#오디오와 볼륨(퍼센트)로 입력받음
-#+6dB은 원래 오디오의 2배 볼륨
-#-6dB은 원래 오디오의 1/2배 볼륨
+#볼륨 조절 기능함수(오디오세그멘트,볼륨(퍼센트))
 def volumeAdjustment(input_audio,volume_persent):
-    try:
-        #볼륨(퍼센트)가 정수가 아닌것으로 입력되었을때
-        if not isinstance(volume_persent,int):
-            print("정수가 아닙니다. (0 ~ 200)")
-            return None
-        
-        #볼륨(퍼센트)가 200초과로 입력되었을때
-        if volume_persent>200:
-            print("너무 큰 볼륨이 입력되었습니다. (0 ~ 200)")
-            return None
-        
-        #볼륨(퍼센트)가 음수로 입력되었을때
-        elif volume_persent<0:
-            print("볼륨(%)에 음수가 입력되었습니다. (0 ~ 200)")
-            return None
-        
-        #볼륨이 0일때
-        elif volume_persent==0:
-            #음소거 처리
-            adjusted_audio=AudioSegment.silent(duration=len(input_audio))
-        
-        #그외 모든 경우 일 때 (정수 1~200)
-        else:
-            #데시벨 수식에 따라 추가해야할 데시벨 계산
-            volume_dB = 20*math.log10(volume_persent/100)
-            adjusted_audio=input_audio+volume_dB
-
-        return adjusted_audio
     
-    #예상하지 못한 예외의 경우
-    except Exception as e:
-        print("volumeAdjustment() 사용중에 예상하지 못한 오류가 발생하였습니다:",e)
-        return None
+    #AudioSegment클래스의 객체가 아닐때
+    if not isinstance(input_audio,AudioSegment):
+        raise Exception("parameter 1, AudioSegment가 아닙니다.")
+
+
+    #볼륨(퍼센트)이 정수혹은 실수가 아닌것으로 입력되었을때
+    if not (isinstance(volume_persent,int) or isinstance(volume_persent,float)):
+        raise Exception("parameter 2, 정수혹은 실수가 아닙니다. (0 ~ 200)")
+    
+    #볼륨(퍼센트)이 200을 초과하여 입력되었을때
+    if volume_persent>200:
+        raise Exception("parameter 2, 너무 큰 볼륨(%)이 입력되었습니다. (0 ~ 200)")
+    
+    #볼륨(퍼센트)이 음수로 입력되었을때
+    if volume_persent<0:
+        raise Exception("parameter 2, 볼륨(%)에 음수가 입력되었습니다. (0 ~ 200)")
+    
+
+    #볼륨(퍼센트)이 0일때
+    if volume_persent==0:
+        #음소거 처리
+        adjusted_audio=AudioSegment.silent(duration=len(input_audio))
+    #그외 모든 경우 일 때 (정수 1~200)
+    else:
+        #데시벨 수식에 따라 추가해야할 데시벨 계산
+        volume_dB = 20*math.log10(volume_persent/100)
+        adjusted_audio=input_audio+volume_dB
+
+    return adjusted_audio
         
