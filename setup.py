4f2aa559f51f34627d0ccee34a3568c585c78738
rjhl2 <zr250vsd20@gmail.com>
2024-12-06 14:22:54 +0900

    create Initial_Package for test

---
+++ setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="oss_library", 
+    version="0.0.1",
+    description="A library for audio processing",
+    long_description=open("README.md", encoding="utf-8").read(),
+    long_description_content_type="text/markdown",
+    author="Leejaeman1",
+    author_email="zr250vsd21@gmail.com",
+    url="https://github.com/Leejaeman1/OSS_Library",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=["pydub"],
+    python_requires=">=3.12",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
