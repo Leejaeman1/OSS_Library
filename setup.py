3ed1a05896ab8ad63c4aa2e6145ec2f997db1ce7
rjhl2 <zr250vsd20@gmail.com>
2024-12-08 00:30:20 +0900

    structure arrange

--- setup.py
+++ setup.py
@@ -1,22 +1,20 @@
 from setuptools import setup, find_packages
 
-
 setup(
-    name="oss_library", 
-    version="0.0.2",
+    name="OSS_Library",
+    version="1.0.0",
     description="A library for audio processing",
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
-    author="Leejaeman1",
-    author_email="zr250vsd21@gmail.com",
-    url="https://github.com/Leejaeman1/OSS_Library",
-    license="MIT",
     packages=find_packages(),
-    install_requires=["pydub"],
-    python_requires=">=3.12",
+    install_requires=[
+        "pydub>=0.25.1",
+        "ffmpeg-python>=0.2.0",
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
+    python_requires=">= 3.12, < 3.13",
 )
