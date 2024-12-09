ce878400a21e01835faff0199ea817a6824d6fe5
Leejaeman1 <zr250vsd21@gmail.com>
2024-12-09 22:09:01 +0900

    documentation error resolve

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,7 +18,9 @@ release = '1.0.0'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx.ext.autodoc'
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -31,7 +33,5 @@ language = 'Korean'
 html_theme = 'alabaster'
 html_static_path = ['_static']
 
-extensions = [
-    'sphinx.ext.autodoc'
-]
+
 
